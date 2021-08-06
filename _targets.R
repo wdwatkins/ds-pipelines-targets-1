dbf2959a124d0de1ca7bd3f8482205a6ca042e29
David Watkins <wwatkins@usgs.gov>
2021-08-06 09:06:51 -0700

    move outputs in _targets.R

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@ list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "model_RMSEs.csv"),
+    download_data(out_filepath = "1_fetch/out/model_RMSEs.csv"),
     format = "file"
   ), 
   # Prepare the data for plotting
@@ -17,7 +17,7 @@ list(
   # Create a plot
   tar_target(
     figure_1_png,
-    make_plot(out_filepath = "figure_1.png", data = eval_data), 
+    make_plot(out_filepath = "3_visualize/out/figure_1.png", data = eval_data), 
     format = "file"
   ),
   # Save the processed data
